03d762adf573452b9d7459de93a17ba67ad8ccd9
harjotscs <harjotscs@gmail.com>
2021-01-10 21:13:07 +0530

    changed homepage image

--- src/routers/links.js
+++ src/routers/links.js
@@ -46,38 +46,22 @@ router.get("/demo", async (req, res) => {
   }
 });
 
-router.get("/add", async (req, res) => {
-  try {
-    // const csv = "./data.csv";
-    fs.readFile("./data.csv", async (err, data) => {
-      const csv = data.toString("utf8");
-      const json = csv2json(csv, { parseNumbers: true, parseJSON: true });
-      console.log(json);
-      await Student.insertMany(json);
-    });
-    // const student = new Student({
-    //   student_name: "mno",
-    //   roll_number: "xyz007",
-    //   father_name: "abc",
-    //   mother_name: "def",
-    //   health_status: "per",
-    //   class_behaviour: "high",
-    //   interest: "body building",
-    //   remarks: "amazing student",
-    //   total_present: "69/100",
-    //   sub_name: ["English", "Mathematics", "Science"],
-    //   max_marks: [100, 100, 100],
-    //   marks_obtained: [96, 98, 99],
-    //   result_status:"Pass"
-    // });
-    // await student.save();
-    console.log("saved");
-    res.send("uploaded");
-  } catch (e) {
-    console.log(e);
-    res.send("Something Went Wrong <br>" + e);
-  }
-});
+// router.get("/add", async (req, res) => {
+//   try {
+//     fs.readFile("./data.csv", async (err, data) => {
+//       const csv = data.toString("utf8");
+//       const json = csv2json(csv, { parseNumbers: true, parseJSON: true });
+//       console.log(json);
+//       await Student.insertMany(json);
+//     });
+//     console.log("saved");
+//     res.send("uploaded");
+//   } catch (e) {
+//     console.log(e);
+//     res.send("Something Went Wrong <br>" + e);
+//   }
+// });
+
 // router.get("/check", async(res, req)=>{
 //   const student = await Student.find({})
 //   console.log(student)
